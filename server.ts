663aff3a01f11c6e69399c4e0e4bf2efbff3d222
liuyang <yang.liu2@northeastern.edu>
2022-11-20 23:58:41 -0500

    fix server

--- server.ts
+++ server.ts
@@ -1,88 +1,31 @@
 /**
  * @file Implements an Express Node HTTP server.
  */
-// import express, {Request, Response} from 'express';
-// import mongoose from "mongoose";
-// import UserController from "./controllers/UserController";
-// import TuitController from "./controllers/TuitController";
-// import LikeController from "./controllers/LikeController";
-// import FollowController from "./controllers/FollowController";
-// import MessageController from "./controllers/MessageController";
-// import BookmarkController from "./controllers/BookmarkController";
-// import AuthenticationController from "./controllers/auth-controller";
-// require('dotenv').config();
-// const cors = require('cors');
-// const session = require("express-session");
-//
-// // mongoose.connect('mongodb://localhost:27017/fse');
-// mongoose.connect('mongodb+srv://yangliu:yl8596221YL!@tuitproject.vfnfs4p.mongodb.net/?retryWrites=true&w=majority');
-//
-// const app = express();
-//
-// app.use(cors({
-//         credentials: true,
-//         origin:true
-//     }));
-//
-// let sess = {
-//     secret: process.env.SECRET,
-//     cookie: {
-//         secure: false
-//     },
-//     resave: true,
-//     saveUninitialized: true
-// }
-//
-// if (process.env.ENV === 'PRODUCTION') {
-//     app.set('trust proxy', 1) // trust first proxy
-//     sess.cookie.secure = true // serve secure cookies
-// }
-//
-// app.use(session(sess));
-// app.use(express.json());
-//
-//
-// const userController = UserController.getInstance(app);
-// const tuitController = TuitController.getInstance(app);
-// const likeController = LikeController.getInstance(app);
-// const messageController = MessageController.getInstance(app);
-// const followController = FollowController.getInstance(app);
-// const bookmarkController = BookmarkController.getInstance(app);
-// AuthenticationController(app);
-//
-// app.get('/', (req: Request, res: Response) =>
-//     res.send('Hi from FSD1!!!'));
-// app.get('/hello', (req: Request, res: Response) =>
-//     res.send('Hi from FSD2!!!'));
-//
-// /**
-//  * Start a server listening at port 4000 locally
-//  * but use environment variable PORT on Heroku if available.
-//  */
-// const PORT = 4000;
-// app.listen(process.env.PORT || PORT);
 import express, {Request, Response} from 'express';
+import mongoose from "mongoose";
 import UserController from "./controllers/UserController";
 import TuitController from "./controllers/TuitController";
 import LikeController from "./controllers/LikeController";
-import mongoose from "mongoose";
-import MessageController from "./controllers/MessageController";
 import FollowController from "./controllers/FollowController";
+import MessageController from "./controllers/MessageController";
 import BookmarkController from "./controllers/BookmarkController";
 import AuthenticationController from "./controllers/auth-controller";
-import DislikeController from "./controllers/DislikeController";
-
+require('dotenv').config();
 const cors = require('cors');
 const session = require("express-session");
 
-// build the connection string
+// mongoose.connect('mongodb://localhost:27017/fse');
 mongoose.connect('mongodb+srv://yangliu:yl8596221YL!@tuitproject.vfnfs4p.mongodb.net/?retryWrites=true&w=majority');
-// mongoose.connect('mongodb://localhost:27017/tuiter');
 
 const app = express();
 
+app.use(cors({
+        credentials: true,
+        origin:true
+    }));
+
 let sess = {
-    secret:  'somevalue',
+    secret: process.env.SECRET,
     cookie: {
         secure: false
     },
@@ -95,29 +38,22 @@ if (process.env.ENV === 'PRODUCTION') {
     sess.cookie.secure = true // serve secure cookies
 }
 
-app.use(session(sess))
+app.use(session(sess));
 app.use(express.json());
 
-const corsOptions ={
-    origin:true,
-    credentials: true,
-}
-app.use(cors(corsOptions));
 
-app.get('/', (req: Request, res: Response) =>
-    res.send('Welcome!'));
-
-
-
-// create RESTful Web service API
 const userController = UserController.getInstance(app);
 const tuitController = TuitController.getInstance(app);
-const likesController = LikeController.getInstance(app);
-const dislikeController = DislikeController.getInstance(app);
+const likeController = LikeController.getInstance(app);
 const messageController = MessageController.getInstance(app);
 const followController = FollowController.getInstance(app);
 const bookmarkController = BookmarkController.getInstance(app);
 AuthenticationController(app);
+
+app.get('/', (req: Request, res: Response) =>
+    res.send('Hi from FSD1!!!'));
+app.get('/hello', (req: Request, res: Response) =>
+    res.send('Hi from FSD2!!!'));
 
 /**
  * Start a server listening at port 4000 locally
